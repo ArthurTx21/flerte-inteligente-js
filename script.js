e9cb5523366f3d5186462ffe7ac0fd25c4e65249
Arthur H R T <86897533+ArthurTx21@users.noreply.github.com>
2024-04-30 15:05:38 -0300

    primeiro projeto
    cantada fodelastica

---
+++ script.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById('n');
+        function redirecionar() {
+            window.location = "https://www.youtube.com/watch?v=izGwDsrQ1eQ";
+        }
+        function clicou() {
+            alert('NÃO É POSSIVEL DIZER NÃO! CLIQUE EM SIM');
+        }
