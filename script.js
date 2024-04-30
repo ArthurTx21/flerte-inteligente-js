c9e6ded040e6d0fde07167ceda95ea5cfb9cc866
Arthur H R T <86897533+ArthurTx21@users.noreply.github.com>
2024-04-30 18:11:30 -0300

    Delete script.js
    tenho q criar a pasta

--- script.js
+++
@@ -1,7 +0,0 @@
-var btn = document.getElementById('n');
-        function redirecionar() {
-            window.location = "https://www.youtube.com/watch?v=izGwDsrQ1eQ";
-        }
-        function clicou() {
-            alert('NÃO É POSSIVEL DIZER NÃO! CLIQUE EM SIM');
-        }
